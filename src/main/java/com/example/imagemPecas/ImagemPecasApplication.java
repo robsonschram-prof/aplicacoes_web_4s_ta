58ba401457005f7b54c83a322c8f968df963326e
Robson Schram <robson.schram@docente.senai.br>
2026-08-12 21:15:33 -0300

    criação do projeto

---
+++ src/main/java/com/example/imagemPecas/ImagemPecasApplication.java
@@ -0,0 +1,13 @@
+package com.example.imagemPecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagemPecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagemPecasApplication.class, args);
+	}
+
+}
